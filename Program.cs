c9acc8e2f0f040271bd7ebd60b852c87f240bdb6
marbel82 <31281571+marbel82@users.noreply.github.com>
2022-03-14 11:11:04 +0100

    Create new project in VS2019

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace UnremovableDebugArgsProjUserRepro
+{
+    public static class Program
+    {
+        public static void Main(string[] args)
+        {
+            foreach (var a in args)
+            {
+                Console.WriteLine(a);
+            }
+        }
+    }
+}
